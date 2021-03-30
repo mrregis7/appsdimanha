ae9e085565a32c3a98be0d6a879a6b14ff1a66d0
unknown <matheusnit97@gmail.com>
2021-03-30 20:45:39 -0300

    Alteracao classe teste

--- src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
+++ src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
@@ -1,11 +1,28 @@
 package br.edu.infnet.appsdim.aula01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploTeste {
 	public static void main(String[] args) {
-		Aluno aluno = new Aluno();
-		aluno.setId(1);
-		aluno.setNome("Matheus");
-		aluno.setEmail("matheus@gmail.com");
-		System.out.println(aluno);
+		
+		String[] nomes = {"Matheus", "maria", "larissa", "joao"};
+		String[] emails = {"matheus@matheus.com", "maria@maria.com", "larissa@larissa.com", "joao@joao.com"};
+
+		List<Aluno> alunos = new ArrayList<Aluno>();		
+		
+		//Criar objetos: populando a colecao
+		for (int i = 0; i < nomes.length; i++) {
+			Aluno aluno = new Aluno();
+			aluno.setId(i+1);
+			aluno.setNome(nomes[i]);
+			aluno.setEmail(emails[i]);
+			alunos.add(aluno);
+		}
+		
+		//Leitura da colecao
+		for (Aluno item : alunos) {
+			System.out.println(item);
+		}
 	}
 }
